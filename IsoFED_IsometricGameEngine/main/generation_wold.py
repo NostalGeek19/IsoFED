7661c97dfd877324e60f53ad8ea07ba344fa8077
Philip <philalex803@gmail.com>
2026-08-07 15:03:18 +0300

    Update generation_wold.py

--- IsoFED_IsometricGameEngine/main/generation_wold.py
+++ IsoFED_IsometricGameEngine/main/generation_wold.py
@@ -608,7 +608,7 @@ class DualViewRenderer:
         self.big_font = pygame.font.Font(None, 48)
 
 
-        self.corner_label_text = "Pre-release version v04.08.2026"
+        self.corner_label_text = "Pre-release version v07.08.2026"
         self.corner_label_color = (255, 255, 255)
         
         self.current_layer = 0
